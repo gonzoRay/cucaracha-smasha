2d6a5dc3862b4243e52593714244ead8d40f6c64
Reagan Cooper <raygunc@gmail.com>
2019-03-21 18:14:22 -0400

    add logo, basic gameboard grid

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <v-layout column>
+      <v-flex>
+        <img
+          alt="game logo"
+          :height="$vuetify.breakpoint.smAndUp ? '120px' : '80px'"
+          src="./assets/logo.png"
+        >
+      </v-flex>
+      <v-flex>
+        <span
+          :class="['brown--text font-weight-light', $vuetify.breakpoint.smAndUp ? 'display-2' : 'headline']"
+        >
+          cucaracha
+          <strong class="red--text font-weight-strong">smasha!</strong>
+        </span>
+      </v-flex>
+    </v-layout>
+    <Gameboard/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Gameboard from './components/Gameboard.vue';
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    Gameboard
   }
 };
 </script>
---
+++ src/components/Gameboard.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-layout>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-card-actions></v-card-actions>
+        <v-container value="grid-list-sm" fluid>
+          <v-layout row wrap>
+            <v-flex v-for="n in 9" :key="n" xs4>
+              <v-card flat tile>
+                <v-img :src="getRandomCockroachImage()" height="150px"></v-img>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+    name: 'Gameboard',
+    props: {},
+    data: () => ({
+        numberOfRoachOptions:  5
+    }),
+    methods: {
+        getRandomCockroachImage() {
+            const randomImageIndex = Math.floor(Math.random() * this.numberOfRoachOptions) + 1;
+            return require(`../assets/roaches/roach-${randomImageIndex}.jpg`);
+        }
+    }
+}
+</script>
+
+<style>
+</style>
