d60727a4a361939d36e8e57570d19a6936668d90
Reagan Cooper <raygunc@gmail.com>
2019-03-21 12:44:53 -0400

    initial state

--- src/store.js
+++ src/store.js
@@ -1,10 +1,18 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    currentUser: { name: 'reagan' },
+    currentHighScore: 0,
+    highScores: [
+      { name: 'joe', score: 7 },
+      { name: 'sarah', score: 8 },
+      { name: 'frank', score: 6 }
+    ]
+  },
   mutations: {},
   actions: {}
 });
